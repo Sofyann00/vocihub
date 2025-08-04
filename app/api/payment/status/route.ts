1451b15b381130dbcea650122ab129fe4bd36863
Sofyann00 <akibsofyan521@gmail.com>
2025-08-04 12:48:52 +0700

    update

--- app/api/payment/status/route.ts
+++ app/api/payment/status/route.ts
@@ -17,46 +17,46 @@ export async function GET(request: Request) {
     const timestamp = new Date().toISOString()
     
     // Your client ID from Rampable
-    const clientId = '5879d3ea47df480f87dc14c7f9f1e0de1554187'
+    const clientId = '5293441ce1ac477bb38f0e70b92abe103337719'
     
     // Generate signature string
     const stringToSign = `${clientId}:${timestamp}`
     
     // Private key for signing
     const privateKey = `-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDhgq2W4K5QBwNM
-yzs4RcB7RLYm5QuLW2TZHqA0ZcyCoPqGGtJhbJ21yMro70LqH+YG7hfEGhRmpWbX
-rvWen8h6hcKyy0xJ/F0/Y1fORhVx1AgvEggfFU6vtJPTc/hBV/NkCyOAD7I+LoqT
-NJWe7+Je8I4I54W/CfHZ7uqAvylMBuVB1aOtK5m8ooX/26+xvWGNBQECioEhWbma
-cud0ErGneYxACPkwZ70fRZOBPIeD8uet4JzhKJas1pt4lazaO2bJ0oTsJBFxv1rK
-HxYfPx7teZKGLBMWHX+1ZM2qhzJ5c68oG4Nf6v2RrhN4vE+ZlzIc2s3EEoa1MYgd
-8fGr553zAgMBAAECggEAJZ8tAaZTPmOlH8sqAo3UelwwvVVp8yJYtUZAOO2gzEw5
-72SLI5jAyOKKQ4kjEzcmI5LWIS+upp9ChIoaTQ/gHNyy6iwzeoZhMiGLNQ0JJ9fV
-U7EA7NqbqYGJRfYrfQ9EujSHhCZaOLiAAuX7biwcCnW8ZGznqcLpd8+9jKQXqiiO
-Dbu6NvVixBwrHrH4+1cv5IZSBWXdlTp0taJbjvoirv2cGoeGMz1u0e+MExE8MVQs
-j1LFh36WP8jbwO3itGA6ZGWhDbOXA2ZpF82bbGH9Sr/PMTM9HoM4PzDY7298pzKL
-32mdHdIM8NzhKisPgPjh41w+8XnxBW19HkSJQeLfHQKBgQD9u5iygnOLH4bgNnpT
-t2Ow/gLW8qMv76hZWOrQgGdC5VQVwLpU0k45+1Q3n/td0K+qwiS67iScOW3FsG4I
-1RgVzqwLzi4lAS7m1RhDUgqnTz+0iuykihWf4v/C6U5uyinKusJKuLx7AqwNUexC
-bOh7/5kmPvzxJhU/F/HQfxp97QKBgQDjhoYu4e7vVRNUhNU1gD2uRVGYnoXJErED
-uHx7vhHgyHAa+rWK3P1+5M8vftZLRlqwol6FieH5n2QFxj5Kd067WWRqr6QS7tnY
-XUAo4mibVLnr0a1mtYNO7LjKu8ec4gjPh+aPl4B9LOUDWjykkguoWktaPF+8vvIU
-ds6PnDoPXwKBgAVJ4bbmzS27LH6D5CzWh7dqs/hy+j/HiQTDu7E42+4jBYumhru5
-3nOK74hxRzX/YYn3nfhfelffQpB6SvDYMDZUJnXgiEmxKp8ZFfZhQWOXXUaQFrqf
-PjAk9RvE7SOWByT4m5AfJZ6Swa9NY9VGm9npy/FmIrOMDalRAHNOEulJAoGBAL7F
-PqD0o0hdBvYo4jQXsJ/8UywqTnaHi5BZEVcXyRm2NfkJGUqzfID3DJkAOmGnPcc4
-fRRuxeBtOop07Cm323XOwBmL54BQcKuvlGozXA5RfgoyJrglnVGBvneN9xKdm1GW
-20SufPd3uxWShaJfKhMgkmm4kXVKjOOHsi5LiYr1AoGBALjtdT6qMKafR7AKGf/6
-z4jQE8U3TgU/6H+r7GgysY4wednOz98585BMBJbviTzeDLUWjpu0cG/54gnMNQKM
-e93CxROfKOS9zkRb1tu9AbuBUg9aNqQw9d8jx3cHYJHEx3rPiXAmEifBCvG7PeL/
-VZLJs+xxwAgjusTOGAQU8mp7
+MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDeOA1T9Qvd3jpb
+V6a/jPlx1aS1lZlFaFpY47lCtVvR+mx3GJRYXD47HgvaFDZUM+5pe9M5NGhGIEqV
+LTjxCIbdyt6IW1+8ZhTMyGtWtU5ngoEZkkfZGt2tSySzuRclYTrDOipHc2q+ZOZ/
+K26zfMbQdcQ5UqRfkWehR0vjnDtx7U1BiNFMoPyW47P2DZiu6uJ5QR5HE+b3ufXn
+lTruiHqJUj6P6JDcIB6uKvsDCQq36pZmQ9TH4oGO+4xnTak4smFeFbd8VI0se6+t
+bxI4/X4014+CcW6fXu30s0ZxlMoilX8Cbg0XvgRjZvgkU2TiMEcw21shKVoqBgvL
+04uoE0DtAgMBAAECggEAL6OZGd1f+qPMY24mSYzdoh2QxrvoRKSzL7i8K/So11pm
+G98ZWFsIHVDa7hjkr8cFTrhUxz/4tcXw70unWCcBkLVP4TAjlUZWIbO6rTdHtHGa
+HyMIw56alO7V0rC0qVcPi7BhxXkVEnWbo3oqbJCsp56Dn11b70ADFSRtfMFuzgSQ
+5hm8v76yTab/Kznnqle0Px14vEjPT0OiyRMFFe/nVXor9A+B2Plc2kdFn+va/la8
+q8t0OVWDdo7I+pUo4eRg0vcvCP60nxi4fjel3I725oGQQ28IzzCwSzQoH3cA+0qT
+g0f30VNCQOK1K/Eo5muRAEf4RsB8GlJmWxtuiGfAGQKBgQD6nMJMURMdnSP5d7w0
+0R3WgLHaFgy30Ft7IaxvT2S6YXA6BXalKim5CBDHu1ak0/Ibg7tMeGv09f4Z3xVD
+xnJtmT+GONh3XlIYqKT7qFdooPLZLaHI6LDX6aXMDkspnWXoJNbE62rk55jLwJ+o
+juXrljB6vWiDcLL4nAtfb8pAaQKBgQDi/wfRKQaNWAchbupSFTHwbb5pSg7r/W4R
+FBuxV95YuNueb3XhgDl7D3n53L6mCwE9VE/WbEjJUVO2wPngUFl50Z5Zh1gkZ2dV
+pvG+oHxnkpUIFrxpIRz/VlaInJTERzAhYktpnxoKMcBjYKq0mk6p+a5ljIprgZT8
+bbSHKeYr5QKBgQCNLH0vbIRuLDU1tMNqpVo/mWOWzNEBQvgllsSCN6EwdO85YBaD
+2AqE1wxM1zVpyJMb+MsrurJl3aT0gfyCn2maX7evLpqSM6nfhjDaKuyCXhgvS0tV
+1+620kG5f1vL54SejxIE//DJXcVvEeVemJDgc0x/9+7GDFhlP9IQQvMV8QKBgQCv
+HGhuXUS1swvFKehPrBQbUr3KQZxjRt0rBaUbWRqovOqHPZrle+Jb4aOKMlVMiD7L
+jggM9Pnte/Sljhb1iWaNCD0s+bDwXGil9aSWFv4BdF3NoKR2QVo6S+Nzfb9x7yzd
+BBQDBf6zkI/G2iK2Mjeu/nMf2ZxKtOs3FHZostTiVQKBgQCNkr2YvHyi4r1pPabg
+mdhnIA8qynZrhPC3CWeXdL5Z3Qoja+6zvAe7qIJKkfObcBLyDZI5UR364FNhzZx7
+eP/EmsvWh421zt+D4GzgWgndom169WVgYqz4nWDVZYFY1cJqVmSwbdxEgmx/JrR0
+RN7VeOMYKYv8bPnlur1dVr7Pog==
 -----END PRIVATE KEY-----`
     
     const sign = crypto.createSign('SHA256')
     sign.update(stringToSign)
     const signature = sign.sign(privateKey, 'base64')
 
-    const response = await fetch(`https://sandbox.mayaramp.com/v1/deposit/${depositId}`, {
+    const response = await fetch(`https://api.mayaramp.com/v1/deposit/${depositId}`, {
       headers: {
         'X-Bypass-Secret': 'YZKNsyPLlvQJ5FwRNPrnLo0UsQZzEOgm',
         'X-SIGNATURE': signature,
